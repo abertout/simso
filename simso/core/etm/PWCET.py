d16685ba71352427adf3a5d81a5bd5ba130aa1a0
Antoine Bertout <antoine.bertout@inria.fr>
2016-09-19 18:14:52 +0200

    add of the probabilistic WCET execution time model

---
+++ simso/core/etm/PWCET.py
@@ -0,0 +1,60 @@
+from simso.core.etm.AbstractExecutionTimeModel \
+    import AbstractExecutionTimeModel
+import random
+
+# TODO: the seed should be specified in order to evaluate on identical systems.
+# More precisely, the computation time of the jobs should remain the same.
+
+
+class PWCET(AbstractExecutionTimeModel):
+    def __init__(self, sim, _):
+        self.sim = sim
+        self.et = {}
+        self.executed = {}
+        self.on_execute_date = {}
+
+
+    def init(self):
+        pass
+
+    def update_executed(self, job):
+        if job in self.on_execute_date:
+            self.executed[job] += (self.sim.now() - self.on_execute_date[job]
+                                   ) * job.cpu.speed
+
+            del self.on_execute_date[job]
+
+    def on_activate(self, job):
+        self.executed[job] = 0
+        pet = next(x[0] for x in job.task.pwcet if random.random() <= x[1]) * self.sim.cycles_per_ms
+        self.et[job] = pet
+        job.et = pet
+
+
+    def on_execute(self, job):
+        self.on_execute_date[job] = self.sim.now()
+
+    def on_preempted(self, job):
+        self.update_executed(job)
+
+    def on_terminated(self, job):
+        self.update_executed(job)
+        del self.et[job]
+
+    def on_abort(self, job):
+        self.update_executed(job)
+        del self.et[job]
+
+    def get_executed(self, job):
+        if job in self.on_execute_date:
+            c = (self.sim.now() - self.on_execute_date[job]) * job.cpu.speed
+        else:
+            c = 0
+        return self.executed[job] + c
+
+    def get_ret(self, job):
+        return int(self.et[job] - self.get_executed(job))
+
+    def update(self):
+        for job in list(self.on_execute_date.keys()):
+            self.update_executed(job)
